cab67cf8b8a763dfa6592599885ca984947a5eb2
Darkwood <verkadehugo@gmail.com>
2026-03-09 16:21:11 +0100

    aaa

--- DebugRemote.c
+++ DebugRemote.c
@@ -1,64 +1,8 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "ssd1306.h"
+#include "hardware2.h"
 
-#define SDA_PIN 8
-#define SCL_PIN 9
-
-#define ROTSTATSTART 10
-#define ROTSTATEND 17
-
-void init_rot_status(void){
-    for(int pin = ROTSTATSTART; pin < ROTSTATEND + 1; pin++){
-        gpio_init(pin);
-        gpio_set_dir(pin, true);
-        gpio_put(pin, false);
-    }
-    
-    gpio_put(ROTSTATSTART, true);
-    gpio_put(ROTSTATSTART+ 2, true);
-    gpio_put(ROTSTATSTART + 5, true);
-    gpio_put(ROTSTATSTART+ 6, true);
-    
-}
-
-void rot1_on(void){
-    gpio_put(16, false);
-    gpio_put(17, true);
-}
-
-void rot1_off(void){
-    gpio_put(17, false);
-    gpio_put(16, true);
-}
-
-void rot2_on(void){
-    gpio_put(16, false);
-    gpio_put(17, true);
-}
-
-void rot2_off(void){
-    gpio_put(17, false);
-    gpio_put(16, true);
-}
-void rot3_on(void){
-    gpio_put(16, false);
-    gpio_put(17, true);
-}
-
-void rot3_off(void){
-    gpio_put(17, false);
-    gpio_put(16, true);
-}
-void rot4_on(void){
-    gpio_put(16, false);
-    gpio_put(17, true);
-}
-
-void rot4_off(void){
-    gpio_put(17, false);
-    gpio_put(16, true);
-}
 int main()
 {
     stdio_init_all();
@@ -75,21 +19,29 @@ int main()
 
     ssd1306_clear();
 
-    for(int x=0;x<128;x++)
-        ssd1306_pixel(x,32,1);
 
     ssd1306_update();
 
     init_rot_status();
+    init_status_leds();
     sleep_ms(1000);
-    rot1_on();
-    sleep_ms(1000);
-    rot2_on();
-    sleep_ms(1000);
-    rot3_on();
-    sleep_ms(1000);
-    rot4_on();
-    while(1)
+    
+    while(1){
+        rot1_on();
         sleep_ms(1000);
+        set_status(READY);
+        rot2_on();
+        sleep_ms(1000);
+        set_status(CONECTING);
+        rot3_on();
+        sleep_ms(1000);
+        set_status(ERROR);
+        rot4_on();
+        sleep_ms(1000);
+        set_status(BUSSY);
+        allrot_off();
+        sleep_ms(1000);
+    }
+    
         
 }
